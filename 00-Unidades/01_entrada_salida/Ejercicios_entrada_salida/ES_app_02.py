a2990de9d77a13527d507b9d4afbae3efd1dad16
Tamara Rauna <tamara.rauna@gmail.com>
2024-02-10 15:44:44 -0300

    U1 - Entrada y salida - Resolución ejercicio 2

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Tamara
+apellido: Rauna
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -21,14 +21,13 @@ class App(customtkinter.CTk):
         super().__init__()
         
         self.title("UTN FRA")
-       
         self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
 
     def btn_mostrar_on_click(self):
-        pass
-        
+        nombre_ingresado = prompt("Ingrese su nombre", "Nombre")
+        alert("Su respuesta", nombre_ingresado)
         
     
 if __name__ == "__main__":
